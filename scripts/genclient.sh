1f8b450f0d5ec69573ad26a5f99db5ad5bb016a0
obesbash <38809133+obesbash@users.noreply.github.com>
2020-04-11 13:05:25 +0300

    Update genclient.sh

--- scripts/genclient.sh
+++ scripts/genclient.sh
@@ -8,7 +8,7 @@ FILE_NAME=client.ovpn
 FILE_PATH="$CLIENT_PATH/$FILE_NAME"
 echo "$(datef) $FILE_PATH file has been generated"
 
-echo "$(datef) Config server started, download your $FILE_NAME config at http://$HOST_ADDR/"
+echo "$(datef) Config server started, download your $FILE_NAME config at http://$HOST_ADDR:8099/"
 echo "$(datef) NOTE: After you download you client config, http server will be shut down!"
 
 { echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat "$FILE_PATH"; } | nc -w0 -l 8080
